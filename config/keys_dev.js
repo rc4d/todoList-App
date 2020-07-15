966e5f7b8702f3f1c92cffee87e60dc70d814e27
robingupta46 <guptarobin466@gmail.com>
2020-07-15 13:40:46 +0530

    upload

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    mongoURI: 'mongodb+srv://robin:robin123@robinproject.ibrj9.mongodb.net/robin?retryWrites=true&w=majority',
+
+}
+
+//This is unnecessary file. you can delete this.
